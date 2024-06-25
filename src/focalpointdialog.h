a1489644a4dac7897e493a1d7312d8a04da12b3a
dkeners <dfrostk06@gmail.com>
2024-06-24 23:09:22 -0400

    Make consistent with other popups and naming, fix image size

--- src/focalpointdialog.h
+++ src/focalpointdialog.h
@@ -7,32 +7,35 @@
 class FocalPointDialog : public wxDialog
 {
 public:
-    FocalPointDialog(wxWindow* parent, const wxString& title, Image* subImage1)
-        : wxDialog(parent, wxID_ANY, title), subImage1(subImage1)
+    FocalPointDialog(wxWindow* parent, const wxString& title, Image* m_subImage)
+        : wxDialog(parent, wxID_ANY, title), m_subImage(m_subImage)
     {
         // Create a wxStaticBitmap to display the image
-        wxStaticBitmap* bitmap = new wxStaticBitmap(this, wxID_ANY, *subImage1);
+        wxStaticBitmap* sb = new wxStaticBitmap(this, wxID_ANY, *m_subImage);
+        m_subImage->setPanelSize(600, 600);
+        m_subImage->FitImage(sb);
 
         // Bind the mouse click event to the OnClick method
-        bitmap->Bind(wxEVT_LEFT_DOWN, &FocalPointDialog::OnClick, this);
+        sb->Bind(wxEVT_LEFT_DOWN, &FocalPointDialog::OnClick, this);
 
         // Use a sizer to layout the controls
         wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
-        sizer->Add(bitmap, 1, wxEXPAND);
+        sizer->Add(sb, 1, wxEXPAND);
         SetSizerAndFit(sizer);
     }
 
 private:
-    Image* subImage1;
+    Image* m_subImage;
 
     void OnClick(wxMouseEvent& event)
     {
         // Get the position of the mouse click
         wxPoint position = event.GetPosition();
+        position.x = position.x / m_subImage->m_scale;
+        position.y = position.y / m_subImage->m_scale;
 
         // Do something with the position...
-        subImage1->setMarkedPoint(position);
-        wxLogMessage("Clicked at %d, %d", position.x, position.y);
+        m_subImage->setMarkedPoint(position);
 
         // Close the dialog
         EndModal(wxID_OK);
