f495636a239b10b262dd6ad5e7ff9cab1a839006
dkeners <dfrostk06@gmail.com>
2024-07-17 16:42:07 -0400

    Add ExportAnimation and docs for both methods

--- src/tools/animator.h
+++ src/tools/animator.h
@@ -18,7 +18,23 @@ namespace Animator {
         InnerEdge,
     };
 
-    // Methods to call a comparer window with the first and second images
-    // void compareImages(wxString firstImage, wxString secondImage);
+    /**
+     * @brief Aligns a frame within an animation.
+     *
+     * This function is responsible for aligning a frame within an animation.
+     *
+     * @param lManager A pointer to the layout manager.
+     * @param image A pointer to the image.
+     * @param animationName The name of the animation. Defaults to `"default"`.
+     */
     void FrameAlignment(LayoutManager* lManager, Image* image, wxString animationName = "default");
+    
+    /**
+     * @brief Exports the animation using the specified layout manager.
+     *
+     * @param lManager Pointer to the main LayoutManager object.
+     * @param animationName (Optional) Name of the animation. Defaults to `"default"`.
+     * @param borderCrop (Optional) Crop the image border. Defaults to `ImageBorderCrop::InnerEdge`.
+     */
+    const int ExportAnimation(LayoutManager* lManager, wxString animationName = "default", ImageBorderCrop borderCrop = ImageBorderCrop::InnerEdge);
 }
